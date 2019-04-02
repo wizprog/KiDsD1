73329420e70afdbfceeb16a8bc733c96d59261b8
Babicuni <Babicuni@DESKTOP-3MG886S>
2019-04-03 00:18:14 +0200

    Adding jsoup and some changes

--- src/components/WebScanner.java
+++ src/components/WebScanner.java
@@ -2,6 +2,8 @@ package components;
 
 import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebScanner implements Callable<Map<String,Integer>> {
 	
@@ -16,6 +18,13 @@ public class WebScanner implements Callable<Map<String,Integer>> {
 	public Map<String,Integer> call() throws Exception {
 		//Scanner
 		return null;
+	}
+	
+	private boolean isUrl(String isUrl) {
+		Pattern pattern = Pattern.compile("^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$");
+        Matcher matcher = pattern.matcher("http://naruto.rs");
+        if(matcher.find()) return true;
+        else return false;	
 	}
 
 }
